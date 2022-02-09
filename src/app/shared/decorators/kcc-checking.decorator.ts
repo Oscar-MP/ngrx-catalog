6d052c0aac9435e3bca63bc6c5fda15e1465db7f
oscar-mp <thegamingesp56@gmail.com>
2022-02-09 13:23:13 +0100

    Removing unnecessary files

--- src/app/shared/decorators/kcc-checking.decorator.ts
+++
@@ -1,9 +0,0 @@
-export function KccChecking( target: any ) {
-    // Do the business to get the cookie and check whatever we need to do.. then
-
-    Object.defineProperty(target.prototype, 'kccCheck', {
-        value: true,
-        writable: false
-    });
-    
-}
