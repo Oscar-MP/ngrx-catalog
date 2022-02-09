6686907ba0ac1cc48917d91f22ffadc4afe9db61
oscar-mp <thegamingesp56@gmail.com>
2022-02-09 13:19:32 +0100

    Content commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { mergeMap } from 'rxjs';
+import { AuthComponent } from './auth/auth.component';
+import { CatalogComponent } from './catalog/catalog.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth', 
+    loadChildren: () => import('./auth/auth.module').then( m => m.AuthModule ),
+    component: AuthComponent
+  },
+  {
+    path: '',
+    loadChildren: () => import('./catalog/catalog.module').then( m => m.CatalogModule ),
+    component: CatalogComponent
+  },
+  {
+    path: 'c',
+    loadChildren: () => import('./catalog/catalog.module').then( m => m.CatalogModule ),
+    component: CatalogComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'c'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StoreModule } from '@ngrx/store';
+import { SharedModule } from './shared/shared.module';
+import { EffectsModule } from '@ngrx/effects';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
